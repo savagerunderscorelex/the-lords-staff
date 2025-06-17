2afdcdb67afe0b701627997e9207961d1e8f1096
savagerunderscorelex <eigbekoyi@gmail.com>
2025-06-17 13:55:40 -0400

    Created options for player (took me a bit to fix my code *cries*)

--- thelordsstaff.py
+++ thelordsstaff.py
@@ -1,5 +1,13 @@
 import random
-
+class Player():
+    damage = 12
+    health = 16
+    money = 100
+    weapon = "Stick"
+x = 2
+def choose():
+    global options
+    options = input("What do you want to do? (1: Scour/ 2: Search for the Lord's Staff in the Maze/3: Upgrade Equipment/4: Check Your Stats)")
 
 print("Welcome to The Lord's Staff! Enter 1 to start the game.")
 enter = input("Enter the Game:")
@@ -7,15 +15,15 @@ if enter == "1":
     print("You spawn in a fantasy city, with witches in crooked hats speeding on their brooms over you, and stone trolls hobbling past you. "
     "You don’t know who you are, where you came from, or what made you come here. " 
     "But you have this overwhelming feeling of heading on a journey to find something. A special something. The Lord’s Staff.")
-choice_go_where = input("What do you want to do? (1: Scour/ 2: Search for the Lord's Staff in the Maze/3: Upgrade Equipment)")
 
-if choice_go_where == 1:
+choose()
+option = options
+
+if option == "1":
     winnings = random.randrange(1,120)
-class Player():
-    damage = 12
-    health = 16
-    money = 100
-    weapon = "Stick"
+    print("You got " + str(winnings) + " moneys!")
+    Player.money += winnings
+    choose()
 
 input_weaponshop = 1
 
