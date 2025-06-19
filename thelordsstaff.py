3ae683d89b25a2fdb702272f6ce37d560b369b8f
savagerunderscorelex <eigbekoyi@gmail.com>
2025-06-19 07:28:03 -0400

    Added shop function

--- thelordsstaff.py
+++ thelordsstaff.py
@@ -1,21 +1,58 @@
 import random
-class Player():
-    damage = 10
-    health = 16
-    money = 100
-    weapon = "Fists"
 
-def choose():
+# Initializing variables, Player class, and functions for gameplay
+class Player():
+    damage = 10 # Base HP damage against monsters and enemies
+    health = 100 # Player's HP, doesn't increase, but can be healed by buying potions
+    money = 50 # Base money, can be increased through "Scouring"
+    weapon = "Fists" # Starter weapon, better ones can be bought in shop
+
+def maze():
+    x = 1
+    """
+    This might not stay a function, it's just a placeholder for the options. Still planning out how the maze will work.
+    """
+def purchasing():
+    print(" ")
+    print("Wares: \nStick (300 coins)\nWooden Sword (500 coins)\nBow (700 coins)\nMetal Sword (1,000 coins)\nDragonblade (7000 coins)")
+    shop_choice = input("What would you like to buy?:")
+    while shop_choice != "Nothing":
+        if shop_choice == "Stick":
+            if Player.money >= 300:
+                confirmation = input("Are you sure?")
+                if confirmation == "Yes":
+                    Player.money -= 300
+                    Player.weapon = "Stick"
+                    print("You just bought the Stick (15 damage)")
+                elif confirmation == "No":
+                    print("idk")
+                    continue
+            else:
+                print("You don't have enough money.")
+def choose(): # Player chooses what they want to do (4 options)
     option = input("What do you want to do? (1: Scour/ 2: Search for the Lord's Staff in the Maze/3: Go to Shop/4: Check Your Stats)")
-    if option == "1":
+    if option == "1": # "Scouring"
         winnings = random.randrange(1,120)
         print("You got " + str(winnings) + " moneys!")
         Player.money += winnings
-    elif option == "4":
+    elif option == "4": # Shows Player their Stats
         print(f"Your money: {Player.money}")
         print(f"Your health: {Player.health}")
         print(f"Your current damage: {Player.damage}")
         print(f"Your current weapon: {Player.weapon}")
+    elif option == "2": # Goes to the "main game"
+        maze()
+    elif option == "3": # Shop to buy potions and better weapons
+        print("You enter a stout shop. Potions fill the shelves on both sides of the cramped shop. A small boy sits at the cashier desk, legs swinging back and forth."
+              "He seems too young to own a shop like this, so you assume he's the owner's son. The boy is likely not older than 10, but he has a demeanor that makes him look so much older.")
+        print(" ")
+        if Player.weapon == "Fists": # Different descriptions based on current weapon
+            print("You look around, taking a closer look at the wares. To the left of you, lies a simple"  + " Stick.\nIt looks like it will add a bit to your damage, having 15 damage instead of 10.\nBehind the boy, lies 3 more weapons,"
+                  " A slightly stronger weapon: a Wooden Sword. This has twice the damage as just your hands (20 damage).\nNext to the Wooden Sword lies a Bow, with about 30 damage. "
+            "A Metal Sword, gleaming in the afternoon sun, catches your attention. This weapon has 40 damage. \n")
+            print("You notice the boy lifting another large sword from under the desk. Wide-eyed, you look over the weapon. \"The Dragonblade,\" the boy said in a wispy voice. \n"
+            "Jewels adorn the sword, shining bright like sunlight on the ocean waves. The tip is sharp, enough to pierce anything it touches. This glorious weapon has 200 damage. ")
+            purchasing()
     choose()
 
 
