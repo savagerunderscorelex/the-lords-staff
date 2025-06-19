c6605ed284dbf3b909070c33fe41cf81f15ae87a
savagerunderscorelex <eigbekoyi@gmail.com>
2025-06-19 11:50:46 -0400

    most of this hour was spent grueling over the inventory class im gonna cryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
    at least I finally got it now

--- thelordsstaff.py
+++ thelordsstaff.py
@@ -1,5 +1,11 @@
 import random
-# Initializing variables, Player class, and functions for gameplay
+# Initializing variables, Player class, Weapon Class + instances, and functions for gameplay
+
+class Inventory():
+    def __init__(self, weapons_list):
+        self.weapons_list = []
+        return weapons_list
+
 
 class Weapon():
     def __init__(self, damage, name, cost):
@@ -8,18 +14,21 @@ class Weapon():
         self.cost = cost
     def __str__(self):
         return f"{self.damage}, {self.name}, {self.cost}"
-
-stick = Weapon(15, "Stick", 300)
-wooden_sword = Weapon(20, "Wooden Sword", 500)
-bow = Weapon(30, "Bow", 700)
-metal_sword = Weapon(40, "Metal Sword", 1000)
-dragonblade = Weapon(200, "Dragonblade", 7000)
-
+    
 class Player():
     health = 100 # Player's HP, doesn't increase, but can be healed by buying potions
     money = 50 # Base money, can be increased through "Scouring"
     health_potions = 0 # Health Potions that can be used to recover a player's health
     player_weapon = Weapon(10, "Fists", 0) # Starter weapon + base damage, decided to have both in one variable
+    bag = Inventory(weapons_list=["Fists"+])
+
+# Weapons and their stats: Damage, Name, and shop Cost
+stick = Weapon(15, "Stick", 300)
+wooden_sword = Weapon(20, "Wooden Sword", 500)
+bow = Weapon(30, "Bow", 700)
+metal_sword = Weapon(40, "Metal Sword", 1000)
+dragonblade = Weapon(200, "Dragonblade", 7000)
+# ------------------------------------------------
 
 print(Player.player_weapon)
 def maze():
@@ -29,31 +38,44 @@ def maze():
     """
 def purchasing():
     print(" ")
-    print("Wares: \nStick (300 coins)\nWooden Sword (500 coins)\nBow (700 coins)\nMetal Sword (1,000 coins)\nDragonblade (7000 coins), ")
+    print(f"Wares: \nStick ({stick.cost} coins)\nWooden Sword ({wooden_sword.cost} coins)\nBow (700 coins)\nMetal Sword ({metal_sword.cost} coins)\nDragonblade ({dragonblade.cost} coins), ")
     while 1 == 1:
         shop_choice = input("What would you like to buy?:")
         if shop_choice == "Nothing":
             print("Have a nice day!")
             break
         if shop_choice == "Stick":
-            if Player.money >= 300:
+            if Player.money >= stick.cost:
                 confirmation = input("Are you sure?")
                 if confirmation == "Yes":
-                    Player.money -= 300
+                    Player.money -= stick.cost
                     Player.player_weapon = stick
                     print("You just bought the Stick (15 damage)")
                     break
                 elif confirmation == "No":
-                    print("Look around, then! Maybe you'll find something else you like.")
+                    print("\"Look around, then! Maybe you'll find something else you like,\" the boy says.")
                     continue
             else:
                 print("You don't have enough money.")
         elif shop_choice == "Wooden Sword":
-             if Player.money >= 500:
+             if Player.money >= wooden_sword.cost:
                 confirmation = input("Are you sure?")
                 if confirmation == "Yes":
-                    Player.money -= 500
+                    Player.money -= wooden_sword.cost
                     Player.player_weapon = wooden_sword
+                    print("You just bought the Wooden Sword (20 damage)")
+                    break
+                elif confirmation == "No":
+                    print("Look around, then! Maybe you'll find something else you like.")
+                    continue
+                else:
+                    print("You don't have enough money.")
+        elif shop_choice == "Bow":
+             if Player.money >= bow.cost:
+                confirmation = input("Are you sure?")
+                if confirmation == "Yes":
+                    Player.money -= bow.cost
+                    Player.player_weapon = bow
                     print("You just bought the Wooden Sword (20 damage)")
                     break
                 elif confirmation == "No":
@@ -77,15 +99,19 @@ def choose(): # Player chooses what they want to do (4 options)
     elif option == "2": # Goes to the "main game"
         maze()
     elif option == "3": # Shop to buy potions and better weapons
-        print("You enter a stout shop. Potions fill the shelves on both sides of the cramped shop. A small boy sits at the cashier desk, legs swinging back and forth."
-              "He seems too young to own a shop like this, so you assume he's the owner's son. The boy is likely not older than 10, but he has a demeanor that makes him look so much older.")
+        print("You enter a stout shop. Potions fill the shelves on both sides of the cramped shop. A small boy sits at the cashier desk, legs swinging back and forth.")
+        print("He seems too young to own a shop like this, so you assume he's the owner's son. The boy is likely not older than 10, but he has a demeanor that makes him look so much older.")
         print(" ")
         if Player.player_weapon.name == "Fists": # Different descriptions based on current weapon
-            print("You look around, taking a closer look at the wares. To the left of you, lies a simple"  + " Stick.\nIt looks like it will add a bit to your damage, having 15 damage instead of 10.\nBehind the boy, lies 3 more weapons,"
-            " A slightly stronger weapon: a Wooden Sword. This has twice the damage as just your hands (20 damage).\nNext to the Wooden Sword lies a Bow, with about 30 damage. "
-            "A Metal Sword, gleaming in the afternoon sun, catches your attention. This weapon has 40 damage. \n")
-            print("You notice the boy lifting another large sword from under the desk. Wide-eyed, you look over the weapon. \"The Dragonblade,\" the boy said in a wispy voice. \n"
-            "Jewels adorn the sword, shining bright like sunlight on the ocean waves. The tip is sharp, enough to pierce anything it touches. This glorious weapon has 200 damage. ")
+            print("You look around, taking a closer look at the wares. To the left of you, lies a simple Stick.")
+            print("It looks like it will add a bit to your damage, having 15 damage instead of 10.")
+            print("Behind the boy, lies 3 more weapons, one is slightly stronger weapon: a Wooden Sword.")
+            print("This has twice the damage as just your hands (20 damage)")
+            print("Next to the Wooden Sword lies a basic Bow, with about 30 damage.")
+            print("\nA Metal Sword, gleaming in the afternoon sun, catches your attention. This weapon has 40 damage.")
+            print("You notice the boy lifting another large sword from under the desk. Wide-eyed, you look over the weapon.")
+            print("\"The Dragonblade,\" the boy said in a wispy voice. \n")
+            print("Jewels adorn the sword, shining bright like sunlight on the ocean waves. The tip is sharp, enough to pierce anything it touches. This glorious weapon has 200 damage. ")
             purchasing()
         choose()
 
